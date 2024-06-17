02630f865646968c9467366bd8066dbbbda82dd9
Wesley Chong <cckwes@gmai.com>
2024-06-18 07:53:18 +0800

    initial commit

---
+++ .wezterm.lua
@@ -0,0 +1,23 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.keys = {
+  -- For word jumping
+  { key = "LeftArrow",  mods = "OPT", action = wezterm.action { SendString = "\x1bb" } },
+  { key = "RightArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bf" } }
+}
+
+config.hide_tab_bar_if_only_one_tab = true
+
+-- This is where you actually apply your config choices
+config.font_size = 11.0
+config.line_height = 1.3
+
+-- For example, changing the color scheme:
+config.color_scheme = "Dracula"
+
+-- and finally, return the configuration to wezterm
+return config
---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,97 @@
+return {
+  {
+    "folke/trouble.nvim",
+    -- opts will be merged with the parent spec
+    opts = { use_diagnostic_signs = true },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>ca"] = { desc = "Code action" },
+      },
+    },
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      {
+        "<C-p>",
+        function()
+          require("telescope.builtin").find_files()
+        end,
+        desc = "Find Files",
+      },
+      {
+        "<C-f>",
+        function()
+          require("telescope.builtin").live_grep()
+        end,
+        desc = "Live Grep",
+      },
+    },
+    -- change some options
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
+      },
+    },
+  },
+
+  -- use <Tab> for selection
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local cmp = require("cmp")
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif vim.snippet.active({ direction = 1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(1)
+            end)
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif vim.snippet.active({ direction = -1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(-1)
+            end)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
+  },
+}
