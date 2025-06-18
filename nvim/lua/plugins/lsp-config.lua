0cc35fc81c9196723faa5ae079c5e3f1301c8b28
Wesley Chong <cckwes@gmai.com>
2025-06-18 21:16:14 +0800

    some improvements for rust for neovim

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,33 +1,5 @@
 return {
   {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        rust_analyzer = {
-          settings = {
-            ["rust-analyzer"] = {
-              diagnostics = {
-                enable = true,
-                disabled = { "unresolved-proc-macro" },
-                enableExperimental = true,
-              },
-              cargo = {
-                allFeatures = true,
-              },
-              checkOnSave = {
-                command = "clippy",
-              },
-              procMacro = {
-                enable = true,
-              },
-              -- Add other rust-analyzer settings here
-            },
-          },
-        },
-      },
-    },
-  },
-  {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
@@ -35,6 +7,33 @@ return {
         "typescript-language-server",
         "gopls",
         "rust-analyzer",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      diagnostics = {
+        virtual_text = false,
+        update_in_insert = true,
+        float = {
+          spacing = 4,
+          border = "rounded",
+          focusable = true,
+          source = "if_many",
+        },
+        severity_sort = true,
+      },
+      codelens = { enabled = true },
+      setup = {
+        bacon_ls = function()
+          require("lspconfig").bacon_ls.setup({
+            init_options = {
+              useCargoBackend = true,
+            },
+          })
+          return true
+        end,
       },
     },
   },
